9fe0eb29fb8ccb60734bba444d941b9e86b21a38
meghnadas <meghnadas2504@gmail.com>
2025-07-12 22:17:04 +0530

    .

--- components/BookCover.tsx
+++ components/BookCover.tsx
@@ -16,19 +16,19 @@ interface Props {
     className?: string;
     variant?: BookCoverVariant;
     coverColor: string; 
-    coverUrl: string
+    coverUrl: string  
 } 
 
 const BookCover = ({ className, variant = "regular", coverColor = "#012B48", coverUrl = "https://palcehold.com/400*600.png" }: Props) => {
     return (
-        <div className={cn(inputs: 'relative transition-all duration-300', variantStyles[variant])}>
+        <div className={cn('relative transition-all duration-300', variantStyles[variant])}>
             <h2 className='font-bebas-neue text-4xl text-light-100'>
-                Book side SVG
+                Book side SVG 
             </h2> 
             <div className='absolute z-10' style={{left:'12%', width:'87.5%',height:"88%" }}>
-                <Image src={coverUrl} alt='Book cover' fill className='rounded-sm object-fill'/>
+                <Image src={coverUrl} alt='Book cover' fill className='rounded-sm object-fill'/> 
             </div>
         </div> 
     ) 
-} 
-export default BookCover   
+}
+export default BookCover; 
